7278efab4e6d111092d6b5fa3944d4e8003ec441
rebreather <yds04116@gmail.com>
2024-08-28 16:20:05 +0900

    chore: 더미 데이터 넣기

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,3 +6,9 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+
+export function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max= Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
