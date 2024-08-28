35298cd48cbfcab26f5e769a74175cb1ef763af0
rebreather <yds04116@gmail.com>
2024-08-28 17:04:54 +0900

    feat: Logo 구현

--- components/elements/Logo.jsx
+++ components/elements/Logo.jsx
@@ -1,9 +1,29 @@
+'use client'
+
 import React from 'react'
+import Image from 'next/image'
+import { RxHamburgerMenu } from 'react-icons/rx'
+import { useRouter } from 'next/navigation'
+import IconButton from './IconButton'
 
 const Logo = () => {
-  return (
-    <div>Logo</div>
-  )
+
+  const { push } = useRouter()
+
+  function onClickLogo() {
+    push('/')
+  }
+
+  function onClickIcon() {
+    //
+  }
+
+  return <section className='flex flex-row items-center gap-3'>
+    <IconButton onClickIcon={onClickIcon} icon={<RxHamburgerMenu size={24} />} />
+    <div className='cursor-pointer' onClick={onClickLogo}>
+      <Image alt='logo' width={100} height={30} src={'/main-logo.svg'} />
+    </div>
+  </section>
 }
 
 export default Logo
